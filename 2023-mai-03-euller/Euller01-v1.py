4d71942164cd135a15b52b3cf5051c9f2341b14c
GALILEU Batista <gbatmobile@gmail.com>
2023-05-05 19:57:41 -0300

    Complemento Euller prob01

--- 2023-mai-03-euller/Euller01-v1.py
+++ 2023-mai-03-euller/Euller01-v1.py
@@ -10,7 +10,7 @@ x = 1
 
 while x < 1000:
     if (x % 3 == 0) or (x % 5 == 0):
-        soma += x
+        soma = soma + x
     x = x + 1
 
 print ("A soma dos múltiplos de 3 ou 5, menores do que 1000 é:", soma)
